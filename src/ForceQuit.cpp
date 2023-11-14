d06ced90ee936826db93466a46e331c28a68b358
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-11-14 02:09:01 +0100

    Add main application logic

--- src/ForceQuit.cpp
+++ src/ForceQuit.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <windows.h>
 
-int main()
+static void terminate_foreground()
 {
-	std::cout << "Hello World!\n";
+	DWORD pid;
+	GetWindowThreadProcessId(GetForegroundWindow(), &pid);
+
+	HANDLE hnd = OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, TRUE, pid);
+
+	TerminateProcess(hnd, 0);
+	CloseHandle(hnd);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (!RegisterHotKey(NULL, 1, MOD_WIN | MOD_NOREPEAT, VK_F4))
+	{
+		return 1;
+	}
+
+	MSG msg{};
+	while (GetMessage(&msg, NULL, 0, 0) != 0)
+	{
+		if (msg.message == WM_HOTKEY)
+		{
+			terminate_foreground();
+		}
+	}
+
+	UnregisterHotKey(NULL, 1);
+	return 0;
 }
